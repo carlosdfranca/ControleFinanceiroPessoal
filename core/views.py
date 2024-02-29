8a3f706869abe44d0f6bbb71fb16fb9c52583c5b
Carlos Eduardo <carlosfranca.dev@gmail.com>
2024-02-28 22:15:22 -0300

    Ideia de Dashboard (Final)

--- core/views.py
+++ core/views.py
@@ -4,5 +4,5 @@ from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
-class DashboradView(TemplateView):
-    template_name = 'dashboard.html'
+class HomepageView(TemplateView):
+    template_name = 'homepage.html'
