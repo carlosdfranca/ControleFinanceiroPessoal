8934b39ad5f6765515470b6d757f0f6933356a79
Carlos Eduardo <carlosfranca.dev@gmail.com>
2024-03-08 19:42:32 -0300

    Tentando Colocar a troca de senha

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -1,7 +1,12 @@
 from django import forms
+from django.contrib.auth.forms import PasswordChangeForm
 from .models import Usuario
 
 class UsuarioForm(forms.ModelForm):
     class Meta:
         model = Usuario
         fields = ['first_name', 'last_name', 'imagem_perfil', 'saldo_inicial']
+
+class TrocarSenhaForm(PasswordChangeForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import UpdateView
+from django.views.generic import UpdateView, FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Usuario
-from .forms import UsuarioForm
+from .forms import UsuarioForm, TrocarSenhaForm
 from django.contrib import messages
+from django.urls import reverse_lazy
+from django.contrib.auth import update_session_auth_hash
 
 # Create your views here.
 class PerfilView(LoginRequiredMixin, UpdateView):
@@ -18,12 +20,26 @@ class PerfilView(LoginRequiredMixin, UpdateView):
         return self.request.path
     
     def form_valid(self, form):
-        
-        usuario = form.save(commit=False)
-        if 'nova_imagem_perfil' in self.request.FILES:
-            usuario.imagem_perfil = self.request.FILES['nova_imagem_perfil']
-        usuario.save()
-
         messages.success(self.request, 'Alterações salvas com sucesso!')
         return super().form_valid(form)
+    
+    
+class PasswordChangeView(FormView):
+    template_name = 'perfil.html'
+    form_class = TrocarSenhaForm
+
+    def get_success_url(self):
+        return self.request.path
+
+    def form_valid(self, form):
+        user = form.save()
+        update_session_auth_hash(self.request, user)  # Atualiza a sessão do usuário para evitar logout
+        messages.success(self.request, 'Senha Alterada!')
+        return super().form_valid(form)
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
 
