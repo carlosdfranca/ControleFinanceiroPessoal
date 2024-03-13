a8238865ca468687449476504154bcf9dcc2eda4
Carlos Eduardo <carlosfranca.dev@gmail.com>
2024-03-12 22:53:56 -0300

    Criando a função de Criar a conta

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomepageView
+from .views import HomepageView, CriarConta
 from django.contrib.auth.views import LoginView, LogoutView
 
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', HomepageView.as_view(), name='homepage'),
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('register/', CriarConta.as_view(), name='register'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,27 @@
 from django.http.response import HttpResponse as HttpResponse
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, FormView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import CriarContaForm
+from django.contrib import messages
+
+
 
 # Create your views here.
+class CriarConta(CreateView):
+    form_class = CriarContaForm
+    template_name = 'register.html'
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Conta criada com sucesso! Você já pode fazer login.')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Ocorreu um erro durante o registro. Por favor, verifique os dados fornecidos.')
+        return super().form_invalid(form)
+
+
+
 class HomepageView(LoginRequiredMixin, TemplateView):
     template_name = 'homepage.html'
